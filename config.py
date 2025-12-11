29c3ad7086f26146f80cfadcf3b7b8c4f6db396a
pk3011 <118091725+pk3011@users.noreply.github.com>
2025-12-11 10:06:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,15 +9,15 @@ load_dotenv("config.env", override=True)
 
 class TgConfig:
     # Telegram related configurations
-    API_ID = int(os.environ.get("API_ID", "123456"))
-    API_HASH = os.environ.get("API_HASH", "your_api_hash")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")
-    FILEDB_CHANNEL = int(os.environ.get("FILEDB_CHANNEL", "-1001234567890"))
+    API_ID = int(os.environ.get("API_ID", "1747265"))
+    API_HASH = os.environ.get("API_HASH", "API_HASH=47f031293f55361c76c27da11916179b")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8173103912:AAFus6z_SSJmg6C-X0WX99qTRD5j-p9eMMU")
+    FILEDB_CHANNEL = int(os.environ.get("FILEDB_CHANNEL", "-1003348559182"))
    
 
 class ServerConfig:
     # Server related configurations
-    DOMAIN_URL = os.environ.get("DOMAIN_URL", "")
+    DOMAIN_URL = os.environ.get("DOMAIN_URL", "https://file-xor-web-3m61.onrender.com")
     DOWNLOAD_PATH = os.environ.get("DOWNLOAD_PATH", "./downloads")
     MAX_FILE_SIZE = int(os.environ.get("MAX_FILE_SIZE", "2048"))  # in MB
     GEN_SECRET_KEY_LENGTH = int(os.environ.get("GEN_SECRET_KEY_LENGTH", "8"))
@@ -41,9 +41,9 @@ class BotInfoConfig:
     # Bot related configurations    
     BOT_NAME = os.environ.get("BOT_NAME", "FILE XOR")
     BOT_LOGO = os.environ.get("BOT_LOGO", "https://files.catbox.moe/8cl88o.jpg")
-    OWNER_ID = int(os.environ.get("OWNER_ID", "123456789"))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "6146726705"))
     SUDO = list(map(int, os.environ.get("SUDO", "0").split(',')))
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://frendz:243601@f2l.qwtj78t.mongodb.net/?appName=f2l")
     MODE = os.environ.get("MODE", "private").lower() 
     LANG = os.environ.get("LANG", "en")  # Default language code
 
